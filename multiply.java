9fb274f7ae83c80c2a29340e729e470f7bbe12d6
Mohsin Imam <mohsinimam651@gmail.com>
2021-12-01 19:51:36 +0530

    Added multiply.java

---
+++ multiply.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Multiply {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+
+		System.out.print("Multiply of the entered numbers is : " + (a + b));
+	}
+}
